966bd9ddf6f3b9627419c60cdbb14cd17c55f014
ayushsinghbisht7895-rgb <ayushsinghbisht7895@gmail.com>
2026-03-01 13:08:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+  function appointment(){
+  window.location.href="appoinment.html";
+  
+  
+ }
+    function drportfolia(){
+      window.location.href="drportfolia.html";
+  
+
+  }
+  function Gynaecologist(){
+    window.location.href="Gynaecologist.html"
+  }
+  function openForm() {
+     
+    document.getElementById("appointmentForm").style.display = "block";
+}
+
+function closeForm() {
+  
+    document.getElementById("appointmentForm").style.display = "none";
+}
+
+function emergence(){
+   window.location.href="emergence.html";
+}
