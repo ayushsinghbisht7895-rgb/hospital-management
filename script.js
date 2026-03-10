5dacf851550366323ce93b17e626b910a97f1dfa
ayushsinghbisht7895-rgb <ayushsinghbisht7895@gmail.com>
2026-03-10 21:24:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
   function appointment(){
+
+    
   window.location.href="appoinment.html";
   
   
  }
+
+
+
     function drportfolia(){
       window.location.href="drportfolia.html";
   
@@ -23,4 +28,62 @@ function closeForm() {
 
 function emergence(){
    window.location.href="emergence.html";
+}
+
+
+// Register Patient
+function patientregistration(){
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+
+let patient = {
+name:name,
+email:email,
+password:password
+};
+
+let patients = JSON.parse(localStorage.getItem("patients")) || [];
+
+patients.push(patient);
+
+localStorage.setItem("patients", JSON.stringify(patients));
+
+alert("Registration Successful");
+
+window.location.href="login.html";
+}
+
+
+// Patient Login
+function Login(){
+
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+
+let patients = JSON.parse(localStorage.getItem("patients")) || [];
+
+let found = false;
+
+for(let i=0;i<patients.length;i++){
+
+if(email === patients[i].email && password === patients[i].password){
+
+localStorage.setItem("patientLogin", email);
+
+found = true;
+
+window.location.href=".html";
+
+break;
+
+}
+
+}
+
+if(!found){
+document.getElementById("msg").innerHTML="Invalid Email or Password";
+}
+
 }
